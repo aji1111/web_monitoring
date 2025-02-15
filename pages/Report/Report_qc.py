846e52d0cb8c59edf8fbfb45cdbb9c3a493d4202
aji1111 <68023433+aji1111@users.noreply.github.com>
2025-02-16 02:23:23 +0700

    Update Report_qc.py

--- pages/Report/Report_qc.py
+++ pages/Report/Report_qc.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 def report_tsel_page():
-    st.title("Halaman Report to TSEL")
-    st.write("Ini adalah halaman untuk Report to TSEL.")
+    st.title("Halaman Report to QC")
+    st.write("Ini adalah halaman untuk Report to QC.")
