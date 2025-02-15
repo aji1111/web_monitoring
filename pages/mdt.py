c717fea3d37a65d184884133104d5dadfcc6c62f
aji1111 <68023433+aji1111@users.noreply.github.com>
2025-02-16 02:28:11 +0700

    Update mdt.py

--- pages/mdt.py
+++ pages/mdt.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-def report_tsel_page():
-    st.title("Halaman Report to TSEL")
-    st.write("Ini adalah halaman untuk Report to TSEL.")
+def mdt_page():
+    st.title("Halaman Report to MDT")
+    st.write("Ini adalah halaman untuk Report to MDT.")
