c788fb398b1cadd1aa96c91b08f53c1681502b50
aji1111 <68023433+aji1111@users.noreply.github.com>
2025-02-16 02:28:43 +0700

    Update monitoring_daily2g.py

--- pages/monitoring/monitoring_daily2g.py
+++ pages/monitoring/monitoring_daily2g.py
@@ -1,14 +1,5 @@
-import dash
-from dash import html, Input, Output
-from dash.dependencies import State
-import dash_bootstrap_components as dbc
-import requests
+import streamlit as st
 
-dash.register_page(__name__)
-
-layout = html.Div([
-    html.H1('Ini page Monitoring Daily 2G'),
-    html.Div('This is our page Monitoring Daily 2G'),
-    html.Br(),
-    dbc.Button("Export to PDF", id="export-pdf-btn", color="primary"),
-])
+def report_tsel_page():
+    st.title("Halaman Report to TSEL")
+    st.write("Ini adalah halaman untuk Report to TSEL.")
