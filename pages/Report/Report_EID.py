a01aa680d8356aebe161597650716216bdece51a
aji1111 <68023433+aji1111@users.noreply.github.com>
2025-02-16 02:25:06 +0700

    Update Report_EID.py

--- pages/Report/Report_EID.py
+++ pages/Report/Report_EID.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-def report_tsel_page():
-    st.title("Halaman Report to TSEL")
-    st.write("Ini adalah halaman untuk Report to TSEL.")
+def report_eid_page():
+    st.title("Halaman Report to EID")
+    st.write("Ini adalah halaman untuk Report to EID.")
