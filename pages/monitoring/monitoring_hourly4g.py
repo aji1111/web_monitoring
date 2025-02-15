3967658959dec637df7b3a3f2ea8cda860f842c1
aji1111 <68023433+aji1111@users.noreply.github.com>
2025-02-16 02:29:34 +0700

    Update monitoring_hourly4g.py

--- pages/monitoring/monitoring_hourly4g.py
+++ pages/monitoring/monitoring_hourly4g.py
@@ -1,9 +1,5 @@
-import dash
-from dash import html
+import streamlit as st
 
-dash.register_page(__name__)
-
-layout = html.Div([
-    html.H1('Ini page Monitoring Hourly 4G'),
-    html.Div('This is our page Monitoring Hourly 4G'),
-])
+def monitoring_hourly4g_page():
+    st.title("Halaman Report to Hourly")
+    st.write("Ini adalah halaman untuk Report to Hourly.")
